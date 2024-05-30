86a5b74a644133c7071467144d6f2578dfe8b826
PabloRius <pablogrius@gmail.com>
2024-05-30 20:00:45 +0200

    Using local storage to store current game

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import confetti from "canvas-confetti"
 
 import { Square } from "./components/Square";
@@ -8,16 +8,32 @@ import { checkEndGame, isWinner } from "./logic/board";
 import { WinnerModal } from "./components/WinnerModal";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const fromStorage = window.localStorage.getItem('board')
+    return fromStorage 
+      ? JSON.parse(fromStorage)
+      : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const fromStorage = window.localStorage.getItem('turn')
+    return fromStorage ?? TURNS.X
+  })
   const [winner, setWinner] = useState(null)
+
+  useEffect(() => {
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+  }, [ turn, board ])
 
   const updateBoard = (index) => {
     if (board[index] || winner) return
 
     const newBoard = [...board]
     newBoard[index] = turn
+    const newTurn = turn === TURNS.X ? TURNS.O: TURNS.X
     setBoard(newBoard)
+
+    setTurn(newTurn)
 
     if (isWinner(newBoard, turn)) {
       confetti()
@@ -25,14 +41,15 @@ function App() {
     } else if (checkEndGame(newBoard)) {
       setWinner(false)
     }
-
-    setTurn(prevTurn=>prevTurn === TURNS.X ? TURNS.O: TURNS.X)
   }
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   return (
